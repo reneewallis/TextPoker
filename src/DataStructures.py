e5be4cd4d88cfc6295e3898210dfcdf705cbc207
reneewallis <101682956+reneewallis@users.noreply.github.com>
2023-08-16 01:42:56 +0100

    Add files via upload

---
+++ src/DataStructures.py
@@ -0,0 +1,182 @@
+#uses Linked List structure
+class Queue:
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def enqueue(self, data):
+        node = Queue.Node(data)
+
+        if not self.head:
+            self.head = node
+            self.tail = node
+
+        else:
+            self.tail.next = node
+            self.tail = node
+
+        self.size += 1
+
+        return
+
+    def dequeue(self):
+
+        if not self.tail:
+            raise Exception("Cannot dequeue from an empty queue")
+
+        data = self.head.data
+        self.head = self.head.next
+        self.size -= 1
+
+        return data
+
+    def getSize(self):
+        return self.size
+
+    def isEmpty(self):
+        return self.size == 0
+
+class CircularLinkedList:
+
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def insertHead(self,data):
+        node = CircularLinkedList.Node(data)
+        self.length += 1
+        if not self.head:
+            self.head = node
+            self.tail = node
+
+        else:
+            temp = self.head
+            node.next = temp
+            self.head = node
+
+        self.tail.next = node
+
+    def insertTail(self,data):
+        node = CircularLinkedList.Node(data)
+        self.length += 1
+        if not self.tail:
+            self.head = node
+            self.tail = node
+            self.tail.next = node
+
+        else:
+            node.next = self.tail.next
+            self.tail.next = node
+            self.tail = node
+
+    def getList(self):
+        dataList = []
+
+        if not self.head:
+            return dataList
+
+
+        else:
+
+            current = self.head
+
+            while True:
+
+                dataList.append(current.data)
+
+                current = current.next
+
+                if current == self.head:
+                    break
+
+            return dataList
+
+    def getSet(self):
+        dataSet = set()
+
+        if not self.head:
+            return dataSet
+
+        else:
+            current = self.head
+
+            while True:
+                dataSet.add(current.data)
+                current = current.next
+                if current == self.head:
+                    break
+
+            return dataSet
+
+    def search(self,target,start=None, end=None, key=lambda x:x):
+        if not self.head:
+            return None
+        if not start:
+            current = self.head
+        else:
+            current = start
+
+        if not end:
+            end = start
+
+
+        while True:
+
+            if key(current.data) == target:
+                return current
+
+            else:
+                current = current.next
+                if current == end:
+                    return None
+
+
+
+    def deleteNode(self,target,key=lambda x:x):
+        if not self.head:
+            return False
+
+        if key(self.head.data) == target:
+
+            if self.head.next == self.head:
+                self.head = None
+                self.tail = None
+                self.length -= 1
+                return True
+
+            else:
+                temp = self.head
+                self.tail.next = self.head.next
+                self.head = temp.next
+                self.length -= 1
+                return True
+
+        prev = self.head
+        current = prev.next
+
+        while current != self.head:
+            if key(current.data) == target:
+                prev.next = current.next
+                self.length -= 1
+                return True
+
+            else:
+                prev = prev.next
+                current = current.next
+
+        return False
+
+    def __repr__(self):
+        return self.getList()
---
+++ src/StringFormatting.py
@@ -0,0 +1,54 @@
+def printPaddedInBox(string, boxChar, padding=2):
+    boxSize = len(string) + 4
+    print(boxChar * boxSize)
+    for i in range(2):
+        for _ in range(padding):
+            print(boxChar, " " * (boxSize - 4), boxChar)
+        if i == 1:
+            break
+        print(boxChar, string, boxChar)
+    print(boxChar * boxSize)
+
+
+def printInFancyBox(msg, indent=1, width=None, title=None):
+    lines = msg.split('\n')
+    space = " " * indent
+    if not width:
+        width = max(map(len, lines))
+
+    box = f'╔{"═" * (width + indent * 2)}╗\n'  # upper_border
+    if title:
+        box += f'║{space}{title:<{width}}{space}║\n'  # title
+        box += f'║{space}{"-" * len(title):<{width}}{space}║\n'  # underscore
+    box += ''.join([f'║{space}{line:^{width}}{space}║\n' for line in lines])
+    box += f'╚{"═" * (width + indent * 2)}╝'  # lower_border
+    print(box)
+
+
+def borderedText(lines):
+    if type(lines) == str:
+        lines = lines.splitlines()
+    width = max(len(line) for line in lines)
+    result = ['┌' + '─' * width + '┐']
+    for line in lines:
+        result.append('│' + (line + ' ' * width)[:width] + '│')
+    result.append('└' + '─' * width + '┘')
+    text = '\n'.join(result)
+
+    print(text)
+
+
+def padAndCentreLine(line, width):
+    lineLength = len(line)
+    padding = width - lineLength if lineLength < width else 0
+    padding = padding // 2
+    print("~" * padding + " " + line + " " + "~" * padding)
+
+def printWithSeperators(lines,sepChar):
+    lines = lines.splitlines()
+    print("\n")
+    width = max(map(len, lines))
+    print(sepChar*width)
+    for line in lines:
+        print(line)
+    print(sepChar*width,end="\n\n")
